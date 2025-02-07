af7aed27ed7a939f86e92bc2c56e67a6b9033ac4
dasomoh <dasomoh88@gmail.com>
2025-02-07 20:08:13 +0000

    feat:pagenation actions

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -51,7 +51,12 @@ async function getProduct(id: number) {
     return (
         <div>
             <div className="relative aspect-square">
-                <Image fill src={product.photo} alt={product.title} />
+            <Image
+              className="object-cover"
+              fill
+              src={product.photo}
+              alt={product.title}
+            />
             </div>
             <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
                 <div className="size-10 overflow-hidden rounded-full">
--- src/components/list-product.tsx
+++ src/components/list-product.tsx
@@ -19,7 +19,13 @@ export default function ListProduct({
   return (
     <Link href={`/products/${id}`} className="flex gap-5">
       <div className="relative size-28 rounded-md overflow-hidden">
-        <Image fill src={photo} alt={title} />
+        <Image
+          fill
+          src={photo}
+          className="object-cover"
+          alt={title}
+          sizes="100%"
+        />
       </div>
       <div className="flex flex-col gap-1 *:text-neutral-900">
         <span className="text-lg">{title}</span>
