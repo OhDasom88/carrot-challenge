112f28b673b7b2e5f566477d8c0d8d8a4da6327c
dasomoh <dasomoh88@gmail.com>
2025-02-16 19:15:50 +0000

    fix : 배포시 발생하는 route.ts 가 모듈이 아니라는 오류 대응

--- src/app/(auth)/api/users/route.ts
+++ src/app/(auth)/api/users/route.ts
@@ -1,10 +1,10 @@
-// import { NextResponse, NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-// export async function GET(request: NextRequest) {
-// //   const { email, password } = await request.json();
-//   return NextResponse.json({ message: "Hello, world!" });
-// }
-// export async function POST(request: NextRequest) {
-//   const data = await request.json();
-//   return NextResponse.json(data);
-// }
+export async function GET(_request: NextRequest) {
+//   const { email, password } = await request.json();
+  return NextResponse.json({ message: "Hello, world!" });
+}
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+  return NextResponse.json(data);
+}
